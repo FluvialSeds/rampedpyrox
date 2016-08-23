efe69f54cf8cba9f5b61fc01be0e567936472bd1
Jordon D. Hemingway <jordonhemingway@gmail.com>
2016-08-23 15:07:59 -0400

    updated conf.py for build

--- docs/conf.py
+++ docs/conf.py
@@ -34,9 +34,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
-    'sphinx.ext.napoleon',
-    'rst2pdf.pdfbuilder'
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
