b2d801534aa2a8e2a1707b308bab8f02a8f8ed5a
Jordon D. Hemingway <jordonhemingway@gmail.com>
2016-08-25 17:52:50 -0400

    playing with docstrings

--- rampedpyrox/core/thermogram.py
+++ rampedpyrox/core/thermogram.py
@@ -11,7 +11,6 @@ import pandas as pd
 
 from scipy.interpolate import interp1d
 
-__docformat__ = 'restructuredtext en'
 
 #define function to extract Thermogram variables from 'real_data'
 def _extract_tg(all_data, nT):
@@ -116,7 +115,7 @@ class Thermogram(object):
 
 
 class RealData(Thermogram):
-	__doc__ = '''
+	'''
 	Class for real data thermograms (e.g. data from RampedPyrox experiment).
 
 	Args:
